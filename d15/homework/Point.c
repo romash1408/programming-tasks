d97fea428c41691b3726fece48e411f8b4c85678
Roman Dubinin <romash1408@yandex.ru>
2016-12-17 14:19:47 +0700

    Update Point.c

--- d15/homework/Point.c
+++ d15/homework/Point.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "Point.h"
 
-char* point_print_coordinates(const Point p, char* str)
+char* point_print_coordinates(const Point pt, char* str)
 {
-	return str + sprintf(str, "%d, %d", p.x, p.y);
+	return str + sprintf(str, "%d, %d", pt.x, pt.y);
 }
 	
-char* point_print(const Point p, char* str)
+char* point_print(const Point pt, char* str)
 {
 	str += sprintf(str, "Point (");
-	str = point_print_coordinates(p, str);
+	str = point_print_coordinates(pt, str);
 	return str + sprintf(str, ")");
 }
