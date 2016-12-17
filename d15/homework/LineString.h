74ecf970c45e1a1c1e91152c790e16a14231ef9a
Roman Dubinin <romash1408@yandex.ru>
2016-12-17 14:28:26 +0700

    Update LineString.h

---
+++ d15/homework/LineString.h
@@ -0,0 +1,21 @@
+#ifndef LINESTRING_H
+#define LINESTRING_H
+
+#include <stdio.h>
+#include "Point.h"
+
+static const LS_MAX_POINTS = 100;
+
+typedef struct
+{
+	int size;
+	Point points[MAX_POINTS];
+} LineString;
+
+char* line_string_print_coordinates(const LineString ls, char* str);
+
+char* line_string_print(const LineString ls, char* str);
+
+void line_string_add_point(LineString *ls, const Point pt);
+
+#endif LINESTRING_H
